55087e1dd3855ff1e9d0bcdac0c20384ca10960b
27-aditya <kambleaditya946@gmail.com>
2024-08-10 23:47:32 +0530

    Added the next stage

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+
 height = int(input("Enter the height: "))
 width = int(input("Enter the width: "))
 
@@ -11,24 +12,45 @@ def random_state(height, width):
     for i in range(height):
         for j in range(width):
             random_number = random.random()
-            if random_number >= 0.5:
-                state[i][j] = 1
-            else:
-                state[i][j] = 0
+            state[i][j] = 1 if random_number >= 0.5 else 0
     return state
     
 def render(state):
     for i in range(len(state)):
         for j in range(len(state[i])):
-            if state[i][j] == 1:
-                print('#', end=' ')
-            else:
-                print('_', end=' ')
+            print('#' if state[i][j] == 1 else '_', end=' ')
         print()
-        
-matrix = random_state(height, width)
-        
-render(matrix)
-    
 
+matrix = random_state(height, width)
+render(matrix)
+
+def next_state(matrix):
+    height = len(matrix)
+    width = len(matrix[0])
+    state = dead_state(height, width)
     
+    # Direction vectors for the eight neighbors
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    
+    for i in range(height):
+        for j in range(width):
+            neighbours = 0
+            for dir in directions:
+                newx = i + dir[0]
+                newy = j + dir[1]
+                if 0 <= newx < height and 0 <= newy < width:
+                    neighbours += matrix[newx][newy]
+            
+            # Apply Conway's Game of Life rules
+            if matrix[i][j] == 1:
+                if neighbours < 2 or neighbours > 3:
+                    state[i][j] = 0  # Cell dies
+                else:
+                    state[i][j] = 1  # Cell survives
+            else:
+                if neighbours == 3:
+                    state[i][j] = 1  # Cell becomes alive
+
+    render(state)
+print("<------>")
+next_state(matrix)
